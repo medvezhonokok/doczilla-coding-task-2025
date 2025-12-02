a1eb9de0b6f915edbfa9ec48499190d2b9658667
Michael Kim <nocap239@gmail.com>
2025-12-02 22:11:30 +0300

    + README for task1.

--- task1-liquid-sorting/src/java/Solution.java
+++ task1-liquid-sorting/src/java/Solution.java
@@ -2,52 +2,10 @@ import model.Game;
 
 public class Solution {
     public static void main(String[] args) {
-//        run(new Game(
-//                new Object[][]{
-//                        {1, 2, 1},
-//                        {2, 1, 2},
-//                        {},
-//                        {}
-//                }, 3
-//        ));
-
-//        run(new Game(
-//                new Object[][]{
-//                        {},
-//                        {},
-//                        {},
-//                        {},
-//                        {},
-//                        {},
-//                        {'к', 'о', 'ж', 'з', 'г', 'с', 'ф'},
-//                        {}
-//                }, 10
-//        ));
-
-//        run(new Game(
-//                new Object[][]{
-//                        {"r", "r", "b", "b"},
-//                        {"b", "r", "b", "r"},
-//                        {},
-//                        {}
-//                }, 4
-//        ));
-
-//        run(new Game(
-//                new Object[][]{
-//                        {"r", "g", "b", "y"},
-//                        {"g", "y", "r", "b"},
-//                        {"b", "r", "y", "g"},
-//                        {"y", "b", "g", "r"},
-//                        {},
-//                        {}
-//                }, 4
-//        ));
-
         run(new Game(
                 new Object[][]{
-                        {"r", "b", "r"},
-                        {"b", "r", "b"},
+                        {1, 2, 1},
+                        {2, 1, 2},
                         {},
                         {}
                 }, 3
